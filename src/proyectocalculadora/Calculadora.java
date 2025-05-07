ae007005bbb99a7e2f03ffa714d2e5226bc5979e
Angais <angelbarnesbogado@gmail.com>
2025-05-07 20:50:33 +0200

    Proyecto inicial

---
+++ src/proyectocalculadora/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocalculadora;
+
+/**
+ *
+ * @author AngelDaw
+ */
+public class Calculadora {
+     public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+}
---
+++ src/proyectocalculadora/ProyectoCalculadora.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectocalculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AngelDaw
+ */
+public class ProyectoCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculadora calc = new Calculadora();
+
+        System.out.print("Introduce el primer número: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Introduce el segundo número: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Elige la operación que quieras realizar: 1.Sumar  2.Restar");
+        int opcion = sc.nextInt();
+
+        double resultado;
+        switch (opcion) {
+            case 1:
+                resultado = calc.sumar(num1, num2);
+                System.out.println("Resultado de la suma: " + resultado);
+                break;
+            case 2:
+                resultado = calc.restar(num1, num2);
+                System.out.println("Resultado de la resta: " + resultado);
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+        sc.close();
+    }
+
+}
