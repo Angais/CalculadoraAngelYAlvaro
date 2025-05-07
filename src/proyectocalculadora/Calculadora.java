0ef1ee83630c4b0c3b82e71e29cf9493b4728e2e
AlvaroGIT <alvaromoramora15@gmail.com>
2025-05-07 23:18:03 +0200

    Multiplicación funcional 2

--- src/proyectocalculadora/Calculadora.java
+++ src/proyectocalculadora/Calculadora.java
@@ -16,4 +16,8 @@ public class Calculadora {
     public double restar(double a, double b) {
         return a - b;
     }
+    
+    public double multiplicacion(double a, double b) {
+        return a * b;
+    }
 }
